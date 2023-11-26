cd9d6c0a65cd57642fc568d38af5024d8447f80b
Feko89 <dominofeco89@gmail.com>
2023-11-26 11:16:35 +0100

    web oprava

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <div className='aldrichFont customUnderline absolute z-30 flex items-center gap-4 pb-4 pl-20 pt-16 text-[41.46px] font-normal tracking-[8.29px] text-white'>
       <Image width={41} height={41} src={'/static/globe.png'} alt='globe icon' />
-      <h1>WEB-ANALYSE</h1>
+      <h1><a href=''>WEB-ANALYSE</a></h1>
     </div>
   )
 }
